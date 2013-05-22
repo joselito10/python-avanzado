89e9ab176a66f8075888aa1b61f12d8b15cf845b
joselito10 <joselorenzoh@gmail.com>
2013-05-22 19:32:45 +0200

    Create ventanas.py

---
+++ ventanas.py
@@ -0,0 +1,36 @@
+from gi.repository import Gtk, GdkPixbuf, Gdk
+import os, sys
+
+
+class GUI:
+  def __init__(self):
+
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file("ventanas.glade")
+		self.handlers = {"onDeleteWindow": Gtk.main_quit,
+						"onButtonPressed": self.onButtonPressed,
+						"onCloseAbout": self.onCloseAbout,
+						}
+        
+		self.builder.connect_signals(self.handlers)
+		
+		self.window = self.builder.get_object("window1")
+		self.window.show_all()
+
+	def destroy(self,window):
+		Gtk.main_quit()
+	
+	def onButtonPressed(self,button):
+		windows = self.builder.get_object("aboutdialog1")
+		windows.show_all()
+		
+	def onCloseAbout(self, *args):
+		self.about = self.builder.get_object("aboutdialog1")
+		self.about.hide()
+
+def main():
+	app = GUI()
+	Gtk.main()
+		
+if __name__ == "__main__":
+    sys.exit(main())
