ff8d5d1c506e179c14becf1fe7db28cb1aa43802
joselito10 <joselorenzoh@gmail.com>
2013-05-30 21:10:47 +0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,16 +1,18 @@
 from gi.repository import Gtk, GdkPixbuf, Gdk
 import os, sys
 
-
+"""Utilizamos el metodo para conectar el par sennal (evento) con funcion asociada (callback)
+con el diccionario de variables handlers, idetificador la sennal y valor la funcion"""
 class GUI:
-  def __init__(self):
+	def __init__(self):
 
 		self.builder = Gtk.Builder()
 		self.builder.add_from_file("crud.glade")
 		self.handlers = {"onDeleteWindow": Gtk.main_quit,
-						#"onButtonPressed": self.onButtonPressed,
-						"onAboutDialog": self.onAboutDialog,
-						"onCloseAbout": self.onCloseAbout
+					"onButtonPressed": self.onButtonPressed,
+					"onAboutDialog": self.onAboutDialog,
+					"onCloseAbout": self.onCloseAbout,
+					#"onButtonPressed": self.pintar,
 						}
         
 		self.builder.connect_signals(self.handlers)
@@ -19,7 +21,10 @@ class GUI:
 
 		self.window.connect("delete-event", Gtk.main_quit)
 		self.window.show_all()
-
+		self.label1 = self.builder.get_object("label1")
+		self.label1.hide()
+		self.entry1 = self.builder.get_object("entry1")
+		self.entry1.hide()
 	
 	def onAboutDialog(self, *args):
 		self.about = self.builder.get_object("aboutdialog1")
@@ -27,18 +32,25 @@ class GUI:
 	
 	def destroy(self,window):
 		Gtk.main_quit()
+	#def pintar(self,button):
+		#print "hola mundo"
 	
-	#def onButtonPressed(self,button):
+	def onButtonPressed(self,button):
 		#windows = self.builder.get_object("aboutdialog1")
 		#windows.show_all()
+		self.label1.show()
+		self.entry1.show()
+		print "hola mundo"
 		
 	def onCloseAbout(self, *args):
 		self.about = self.builder.get_object("aboutdialog1")
 		self.about.hide()
 
 def main():
+		
 	app = GUI()
 	Gtk.main()
+	
 		
 if __name__ == "__main__":
     sys.exit(main())
